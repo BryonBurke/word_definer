01b92ef7cb2623f20f31c7ec7ad0014efd0e126d
bryon burke <bryonburke@bryons-Mac-mini.hsd1.or.comcast.net>
2019-12-20 16:20:11 -0800

    rspec error troubleshooting in words.rb

--- lib/Word.rb
+++ lib/Word.rb
@@ -5,3 +5,13 @@ class Word
 
   @@words = {}
   @@total_rows = 0
+
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @id = attributes.fetch(:id) || @@total_rows += 1
+
+  end
+
+  def save
+    @@words[self.id] = Word.new({ :name => self.name, :id => self.id })
+  end
