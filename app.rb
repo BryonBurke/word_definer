cfb1dcec9bfee105303c02a9b06cdc4537e5b9bb
bryon burke <bryonburke@bryons-Mac-mini.local>
2020-01-05 11:57:20 -0800

    Word update feature added

--- app.rb
+++ app.rb
@@ -79,7 +79,7 @@ end
 patch ('/words/:id/definitions/:definition_id') do
   @word = Word.find(params[:id].to_i())
   definition = Definition.find(params[:definition_id].to_i())
-  definition.update(params[:name], @word.id, id)
+  definition.update(params[:name], @word.id)
   erb(:word)
 end
 
